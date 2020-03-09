942bc234c6c359dfa4d56e195f16ba3d2e218f9b
Eric Ampire <ericampire.top@gmail.com>
2020-03-09 18:08:01 +0200

    Snack bar support Support for messages from string resources

--- kutil/src/main/java/com/ericampire/android/kutil/Util.kt
+++ kutil/src/main/java/com/ericampire/android/kutil/Util.kt
@@ -1,7 +1,10 @@
 package com.ericampire.android.kutil
 
 import android.content.Context
+import android.view.View
 import android.widget.Toast
+import androidx.annotation.StringRes
+import com.google.android.material.snackbar.Snackbar
 
 fun Context.toast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
@@ -9,4 +12,28 @@ fun Context.toast(message: String) {
 
 fun Context.longToast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
+
+fun Context.toast(@StringRes message: Int) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.longToast(@StringRes message: Int) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
+
+fun View.snackbar(message: String) {
+    Snackbar.make(this, message, Snackbar.LENGTH_SHORT).show()
+}
+
+fun View.longSnackbar(message: String) {
+    Snackbar.make(this, message, Snackbar.LENGTH_SHORT).show()
+}
+
+fun View.snackbar(@StringRes message: Int) {
+    Snackbar.make(this, message, Snackbar.LENGTH_SHORT).show()
+}
+
+fun View.longSnackbar(@StringRes message: Int) {
+    Snackbar.make(this, message, Snackbar.LENGTH_SHORT).show()
 }
